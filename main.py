cfcae44407dd75d50a823d903ea72a0b20c887f5
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2022-10-14 15:24:29 +0300

    Update main.py
    зачли

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sin,cos
+from tabulate import tabulate
+from matplotlib import ticker
+
+
 def f(x): # функция
     return 3*x-cos(x)+0.1
 def d1(x): # первая производная
@@ -10,35 +14,67 @@ def d2(x): # вторая производная
 def fi(x): # эквивалентная функция фи(х)
     return (cos(x)-0.1) / 3
 # метод простых итераций для решения нелинейных уравнений
-def iterations(x0,x1,e,imax=1000): # e окрестность,imax наибольшее количество итераций x0 начало отрезка x1 конец отрезка
+def iterations(x0,x1,e,imax=100): # e окрестность,imax наибольшее количество итераций x0 начало отрезка x1 конец отрезка
     i = 0
     x = fi(x0)
+    print('=====================================')
+    print('Iterations method')
+    print('function =  3*x-cos(x)+0,1=0')
+    print('fi = (cos(x)-0.1) / 3')
+    print('d/dx fi(x) = -sin(x) / 3   d/dx fi(a) =  ',-sin(x0+ 10**-10) / 3, '  d/dx fi(b) =  ', -sin(x1) / 3)
+    print('Вывод двухсторонняя сход')
+    col_names = ["Iteration", "x","fi(x)","delta"]
+    data = []
     while True:
+
         i = i + 1
         x0 = x
         x = fi(x)
-        if ((abs(x-x0) < e) and (i > imax)) or x0 > x1:
+        if ((abs(x-x0) < e) or (i > imax)) or x0 > x1:
             break
-    return x
+        data.append([i,x0,x,(abs(x-x0))])
+    print(tabulate(data, headers=col_names, tablefmt="fancy_grid",stralign='center'))
+    print('Iterations solution:', x)
 # комбинированный метод решения нелинейных уравнений
 def combined(a,b,e):  # a начало, b конец,e окрестность
+   print('=====================================')
+   print('Combined Method')
+   print('f(x) = 3*x-cos(x)+0,1')
+   print('dy/dx: sin(x) + 3')
+   print('d^2y/dx^2: cos(x)')
+   print('f(a) = {} f(b) = {} f`(a) = {} f`(b) = {} f``(a) = {} f``(b) = {}'.format(f(a),f(b),d1(a),d1(b),d2(a),d2(b)))
+   print('По недостатку - хорды по избытку касательные')
+   col_names = ["Iteration", "!xn ", "f(!xn)","xn","f(xn)","delta"]
+   data = []
+   i = 0
+   xa,xb = a,b
    while True:
+    if i == 0: data.append([i,xa,f(xa),xb,f(xb),abs(xa-xb)])
+    i = i + 1
     if f(a) * d2(a) < 0:
         a = a - (f(a)*(a-b))/(f(a)-f(b))
+        xa = a
     else:
         a = a - f(a)/d1(a)
+        xa = a
     if f(b) * d2(b) < 0:
         b = b - (f(b)*(b-a))/(f(b)-f(a))
+        xb = b
     else:
         b = b - f(b)/d1(b)
-    if abs(a-b) <= 2*e:
+        xb = b
+    x = (a + b) / 2
+    data.append([i, xa, f(xa), xb, f(xb), abs(a - b)])
+    if abs(a-b) <= e:
         break
-    x = (a+b)/2
-   return x
+
+
+   print(tabulate(data, headers=col_names, tablefmt="fancy_grid", stralign='center'))
+   print('Combined solution: ', x)
+
 
 #инициализация параметров
 a,b = 0,0.9
-
 
 # вывод интерфейса
 print('Equation: 3*x-cos(x)+0,1=0')
@@ -47,29 +83,36 @@ print('dy/dx: sin(x) + 3')
 print('d^2y/dx^2: cos(x)')
 
 # решение уравнения 2-мя способами
-iterations_solution = iterations(a,b,0.00001)
-combined_solution = combined(a,b,0.00001)
-
-# вывод решений
-print('Combined solution: ', combined_solution)
-print('Iterations solution: ', iterations_solution)
-# вывод округлённых решений
-print('Rounded combined: ',round(combined_solution,4))
-print('Rounded iterations: ',round(iterations_solution,4))
-
-
+iterations(a,b,0.00001)
+combined(a,b,0.000001)
 
 # график
-x = np.arange(-10,10, 0.00001)
+x = np.arange(-10,10, 0.01)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+plt.plot(x,3*x-np.cos(x)+0.1,label='f(x) = 3x-cos(x) + 0.1')
 
-# отключаем
+
+#  Устанавливаем интервал основных и
+#  вспомогательных делений:
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
 
 
-#subplot 4
-sp = plt.subplot()
-plt.plot(x, 3*x-np.cos(x)  + 0.1)
-sp.spines['left'].set_position('center')
-sp.spines['bottom'].set_position('center')
-plt.title(r'$f(x) =  3*x-cos(x)+0,1$')
+#  Добавляем линии основной сетки:
+ax.grid(which='major',
+        color = 'k')
 
+#  Включаем видимость вспомогательных делений:
+ax.minorticks_on()
+#  Теперь можем отдельно задавать внешний вид
+#  вспомогательной сетки:
+ax.grid(which='minor',
+        color = 'gray',
+        linestyle = ':')
+
+fig.set_figwidth(12)
+fig.set_figheight(8)
+plt.title('f(X) = 3*x - cos(x) + 0.1')
 plt.show()
+
